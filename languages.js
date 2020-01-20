70d05be7c38702c083c3866ebeed8b072110c28f
Serguei S. Dukachev <admin@mfitness.ru>
2020-01-20 14:23:28 +0300

    Create languages.js

---
+++ languages.js
@@ -0,0 +1,17 @@
+//From https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+langList["cz"] = "čeština";//cs
+langList["dk"] = "Dansk"; //da
+langList["de"] = "Deutsch";
+langList["el"] = "Ελληνικά";
+langList["en"] = "English";
+langList["es"] = "Español";
+langList["fi"] = "Suomi";
+langList["fr"] = "France";
+langList["hu"] = "Magyar nyelv";
+langList["it"] = "Italiano";
+langList["nl"] = "Nederlands";
+langList["pl"] = "język polski";
+langList["ru"] = "Русский";
+langList["se"] = "Svenska"; //sv
+langList["si"] = "slovenščina"; //sl
+langList["tr"] = "Türkçe";
