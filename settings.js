97b58ef7706fd6b4dcf9fad79414f326975c11de
Serguei S. Dukachev <github@dukess.ru>
2020-02-25 15:06:16 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,7 @@
-externaldevelop = false; //true - develop on external web server (apache); false - production on arduino web server
-defaultLanguage = 'ru'; // This var overrides automatic language detection
-    defaultLanguage = ''; // Enable autodetection
+externaldevelop = false; //true - develop on external web server (apache); false - production on arduino web server (default)
+defaultLanguage = ''; // defaultLanguage = 'ru' - this var overrides automatic language detection (use ISO 639-1 codes).  defaultLanguage = ''- Enable autodetection (default).
 JSONOptionsPerRequest = 10; //1 - 200. Less: more responsive (interactive), but slower. More: faster, but less responsive
 warnAboutExitWithoutSave = 3; //Warnings before exit from category and discard changes
-CacheCategoriesInRAM = true; //Freeing RAM after exit from category
-functionsListOnFrontPage = [8700, 8741, 8740, 8830, 9005, 8310, 8311, 8314, 8316]; //List of functions for displaying on front (home) page
-monitorUpdateInterval = 60000; //Update values of functions for displaying on front (home) page every ... ms.
+CacheCategoriesInRAM = true; //true - cache categories data in RAM (default). false - freeing RAM after exit from category
+functionsListOnFrontPage = [8700, 8741, 8740, 8830, 9005, 8310, 8311, 8314, 8316]; //List of functions for displaying on front (home) page. Default - empty.
+monitorUpdateInterval = 60000; //Update values of functions for displaying on front (home) page every ... ms. Default - 60000 ms.
