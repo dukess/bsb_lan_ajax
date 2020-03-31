5e61cb6f1a147d2658a694fa8a5d59158f50fb18
Serguei S. Dukachev <github@dukess.ru>
2020-03-31 15:34:51 +0300

    little bit better for small/big displays

--- settings.js
+++ settings.js
@@ -6,3 +6,4 @@ CacheCategoriesInRAM = true; //true - cache categories data in RAM (default). fa
 functionsListOnFrontPage = [8700, 8741, 8740, 8830, 9005, 8310, 8311, 8314, 8316]; //List of functions for displaying on front (home) page. Default - empty.
 monitorUpdateInterval = 60000; //Update values of functions for displaying on front (home) page every ... ms. Default - 60000 ms.
 quickManageFunctionsList = [700, 1600, 1601]; //Quick access to manage these functions.
+displayWelcomeText = true; //Display short review about program functions on start page
