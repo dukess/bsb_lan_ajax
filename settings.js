e41900a3cfd843508d4da32014ba591e28aac5ff
Serguei S. Dukachev <github@dukess.ru>
2020-04-16 22:58:22 +0300

    beautyfulization

--- settings.js
+++ settings.js
@@ -7,3 +7,4 @@ functionsListOnFrontPage = [8700, 8741, 8740, 8830, 9005, 8310, 8311, 8314, 8316
 monitorUpdateInterval = 60000; //Update values of functions for displaying on front (home) page every ... ms. Default - 60000 ms.
 quickManageFunctionsList = [700, 1600, 1601]; //Quick access to manage these functions.
 displayWelcomeText = true; //Display short review about program functions on start page
+hideUnsupportedParams = true; //hide unsupported params. true - hide, false - show. You can show it for debug purposes or other reason
