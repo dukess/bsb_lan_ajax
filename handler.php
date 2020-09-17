65adc82f4e728d4f198138ea8463ae448fa3d4d4
Serguei S. Dukachev <github@dukess.ru>
2020-09-17 16:50:17 +0300

    Using JS command instead WS
    Using JS (POST JSON) command instead WS (GET)
    Chart width depend on window width

--- handler.php
+++ handler.php
@@ -1,4 +1,18 @@
 <?
-$myurl='http://192.168.0.4/' . $_REQUEST['urri'];
-echo file_get_contents($myurl);
+$myurl='http://192.168.0.4/' . $_REQUEST['command'];
+if(strlen($_REQUEST['payload']) > 0){
+$opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/x-www-form-urlencoded',
+        'content' => $_REQUEST['payload']
+    )
+);
+
+$context = stream_context_create($opts);
+echo  file_get_contents($myurl, false, $context);
+}
+else {
+  echo file_get_contents($myurl);
+}
 ?>
